be9c5876738118a5ae62637a137643a9528eb7a0
DESKTOP-LQLDDTH\tomas <tomasfanto@gmail.com>
2022-01-20 15:01:14 +0100

    gitignore

--- kronleinbackend-server/src/main/java/org/kro/api/KroApi.java
+++ kronleinbackend-server/src/main/java/org/kro/api/KroApi.java
@@ -1,6 +1,8 @@
 package org.kro.api;
 
 import java.time.LocalDate;
+
+/**/
 import java.util.List;
 
 import org.kro.bo.CustomerService;
@@ -12,8 +14,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-
 
 @RestController
 @RequestMapping(value = "Api")
